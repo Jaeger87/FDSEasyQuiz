6b777c4053ab2e165c387677b251775414c190a0
Unknown <bad_hunter_666@yahoo.it>
2019-02-26 16:06:55 +0100

    passi avanti recycler

--- app/src/main/java/net/jaegerbox/fdseasyquiz/MainActivity.java
+++ app/src/main/java/net/jaegerbox/fdseasyquiz/MainActivity.java
@@ -2,6 +2,8 @@ package net.jaegerbox.fdseasyquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import org.androidannotations.annotations.AfterViews;
@@ -11,15 +13,29 @@ import org.androidannotations.annotations.ViewById;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
+
     @ViewById
     TextView testText;
 
 
 
+    @ViewById
+    RecyclerView questions;
+
 
     @AfterViews
     void initActivity() {
         testText.setText("hao");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        questions.setLayoutManager(layoutManager);
+
+        /*
+         * Use this setting to improve performance if you know that changes in content do not
+         * change the child layout size in the RecyclerView
+         */
+        questions.setHasFixedSize(true);
     }
 
 }
